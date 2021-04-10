c2ad6eb5966e20b2d0d290389e8ff59fee877499
Aapo Vienamo <aapo.vienamo@iki.fi>
2021-04-10 13:46:24 +0300

    mmio: Add VolatileIO

--- mmio.h
+++ mmio.h
@@ -1,7 +1,27 @@
 #ifndef MMIO_H
 #define MMIO_H
 
+#include <cstdint>
+
 namespace mmio {
+
+template <typename T>
+struct VolatileIO {
+    T read(uintptr_t addr)
+    {
+        return *(volatile T *)addr;
+    }
+    void write(uintptr_t addr, T val)
+    {
+        *(volatile T *)addr = val;
+    }
+};
+
+template <typename T>
+struct BitMask {
+    T clear;
+    T set;
+};
 
 };
 
