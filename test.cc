a39e3fa5033de8d8fba7d7344e8e285e34170efa
Aapo Vienamo <aapo.vienamo@iki.fi>
2021-04-27 22:55:01 +0300

    test: Test ModeField

--- test.cc
+++ test.cc
@@ -20,13 +20,26 @@ enum class TestBits {
     Bit3,
 };
 
+enum class TestModes {
+    Mode0 = 0,
+    Mode1 = 1,
+};
+
 template <typename RegBase>
 using TestBitField = mmio::BitField<RegBase, 2, 0, TestBits>;
-using TestReg = mmio::Register<DummyIO, uint32_t, 0x1, TestBitField>;
+
+template <typename RegBase>
+using TestModeField = mmio::ModeField<RegBase, 1, 2, TestModes>;
+
+using TestReg = mmio::Register<DummyIO, uint32_t, 0x1,
+      TestBitField,
+      TestModeField
+  >;
 
 int main()
 {
     TestReg::set(TestBits::Bit0, TestBits::Bit1);
+    TestReg::set(TestModes::Mode0);
     TestReg::clear(TestBits::Bit0);
     TestReg::read().set(TestBits::Bit2).write();
     TestReg::get(TestBits::Bit0);
