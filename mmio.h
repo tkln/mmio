002abdccadacb1e57f098be4316f8a22b5ac2664
Aapo Vienamo <aapo.vienamo@iki.fi>
2021-04-27 20:20:27 +0300

    mmio: Add BitField

--- mmio.h
+++ mmio.h
@@ -42,6 +42,58 @@ struct ModeField {
     }
 };
 
+template <typename M, typename B>
+static constexpr M get_mask(B bit)
+{
+    return 1U << static_cast<M>(bit);
+}
+
+template <typename M, typename B, typename... Bits>
+static constexpr M get_mask(B bit, Bits... bits)
+{
+    return get_mask<M>(bit) | get_mask<M>(bits...);
+}
+
+template <typename T>
+static constexpr void check_types(){}
+
+template <typename T, typename... Ts>
+static constexpr void check_types(T v, Ts... vs)
+{
+    check_types<T>(vs...);
+}
+
+template <
+    typename IO,
+    typename BackT,
+    uintptr_t addr,
+    unsigned width,
+    unsigned off,
+    typename ValT
+>
+struct BitField {
+    static constexpr BackT mask = ((1U << width) - 1U) << off;
+    using T = ValT;
+
+    template <typename... Bits>
+    static void set(Bits... v)
+    {
+        check_types<ValT>(v...);
+        auto r = IO::read(addr);
+        r |= get_mask<BackT>(v...);
+        IO::write(addr, r);
+    }
+
+    template <typename... Bits>
+    static void clear(Bits... v)
+    {
+        check_types<ValT>(v...);
+        auto r = IO::read(addr);
+        r &= ~get_mask<BackT>(v...);
+        IO::write(addr, r);
+    }
+};
+
 };
 
 #endif
