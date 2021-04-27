80588436450108bbda630ec47b2b284182fe0d19
Aapo Vienamo <aapo.vienamo@iki.fi>
2021-04-27 20:18:46 +0300

    test: Add DummyIO

--- test.cc
+++ test.cc
@@ -1,5 +1,18 @@
 #include "mmio.h"
 
+static uint32_t io_buf[256];
+
+struct DummyIO {
+    static uint32_t read(uintptr_t addr)
+    {
+        return io_buf[addr];
+    }
+    static void write(uintptr_t addr, uint32_t val)
+    {
+        io_buf[addr] = val;
+    }
+};
+
 int main()
 {
 }
